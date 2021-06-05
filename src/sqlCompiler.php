ce8507363ca2704b910bbf8203aaf8a030e14745
ctYurk15 <levgenetic@gmail.com>
2021-06-05 12:02:43 +0300

    Initial commit

---
+++ src/sqlCompiler.php
@@ -0,0 +1,79 @@
+<?php
+
+class SQLCompiler
+{
+    protected $conn;
+    
+    protected $host;
+    protected $db;
+    protected $user;
+    protected $pass;
+    
+    public function __construct($host, $user, $pass, $db)
+    {
+        //initializing connection
+        $this->conn = new mysqli($host, $user, $pass, $db);
+        
+        //storing connection data
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db = $db;
+    }
+    
+    public function getConnectionData()
+    {
+        return [$this->host, $this->user, $this->pass, $this->db];
+    }
+    
+    public function testConnection()
+    {
+        $result = null;
+        
+        $result = (bool)$this->conn->query("SELECT true")->fetch_array()[0];
+        
+        return $result;
+    }
+    
+    protected function sendSQL($query)
+    {
+        $result = $this->conn->query($query);
+        return $result;
+    }
+    
+    public function compile($query)
+    {
+        $result = null;
+        
+        $response = $this->sendSQL($query);
+        
+        //checking if sql response is array or bool
+        if(is_bool($response))
+        {
+            //if request was to change db data
+            if($response)
+            {
+                $result = $response;
+            }
+            //if server returned error
+            else
+            {
+                $result = $this->conn->error;
+            }
+            
+        }
+        else
+        {
+            //if request was to get db data
+            $result = [];
+            while($row = $response->fetch_array())
+            {
+                array_push($result, $row);
+            }
+        }
+        
+        return $result;
+    }
+}
+
+?>
